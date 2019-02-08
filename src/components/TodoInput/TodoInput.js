69a8ebe9023121675ab6e9894ecc3dd1f07eda0d
Navid Farahzadi <na.farahzadi@gmail.com>
2019-02-08 08:46:19 +0330

    extracted todo input to separate component

---
+++ src/components/TodoInput/TodoInput.js
@@ -0,0 +1,37 @@
+// @flow
+import React, { useState } from 'react'
+import './TodoInput.css'
+
+type Props = {
+  onEnterText: Function
+}
+
+function TodoInput(props: Props) {
+  const { onEnterText } = props
+  const [inputText, setInputText] = useState('')
+
+  const handleTextChange = e => {
+    setInputText(e.target.value)
+  }
+
+  const handleKeyDown = e => {
+    const text = inputText.trim()
+    if (e.keyCode === 13 && text) {
+      onEnterText(text)
+      setInputText('')
+    }
+  }
+
+  return (
+    <input
+      type='text'
+      placeholder='enter new todo...'
+      value={inputText}
+      onKeyDown={handleKeyDown}
+      onChange={handleTextChange}
+      className='todo-input'
+    />
+  )
+}
+
+export default TodoInput
--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,36 +1,21 @@
 // @flow
 import React, { useState } from 'react'
 import TodoList from '../TodoList'
+import TodoInput from '../TodoInput'
 import './Todos.css'
 
 type Props = {}
 
 function Todos(props: Props) {
   const [todos, setTodos] = useState([])
-  const [inputText, setInputText] = useState('')
 
-  const handleTextChange = e => {
-    setInputText(e.target.value)
-  }
-
-  const handleKeyDown = e => {
-    const text = inputText.trim()
-    if (e.keyCode === 13 && text) {
-      setTodos([...todos, { id: todos.length + 1, text, done: false }])
-      setInputText('')
-    }
+  const handleEnterText = text => {
+    setTodos([...todos, { id: todos.length + 1, text, done: false }])
   }
 
   return (
     <div className='todos'>
-      <input
-        type='text'
-        placeholder='enter new todo...'
-        value={inputText}
-        onKeyDown={handleKeyDown}
-        onChange={handleTextChange}
-        className='todo-input'
-      />
+      <TodoInput onEnterText={handleEnterText} />
       <TodoList todos={todos} />
     </div>
   )
